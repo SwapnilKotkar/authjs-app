3351f482ea3b3ca0c7139eecaa3167c2607a92be
Swapnil Kotkar <swapnilkotkar793@gmail.com>
2024-08-24 23:03:11 +0530

    fronetned files added/updated

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/shared/Navbar";
+import React from "react";
+
+const page = () => {
+	return (
+		<div className="h-[100vh]">
+			<Navbar />
+			<div>hello</div>
+		</div>
+	);
+};
+
+export default page;
