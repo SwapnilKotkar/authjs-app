68c2260fb6419933d2bbdba640d0c7a9a48c5805
Swapnil Kotkar <swapnilkotkar793@gmail.com>
2024-08-24 22:59:59 +0530

    added types for function params

---
+++ types/index.ts
@@ -0,0 +1,26 @@
+// ====== USER PARAMS
+export type CreateUserParams = {
+	firstName: string;
+	lastName: string;
+	email: string;
+	username: string;
+	password: string;
+	photo?: string;
+	path: string;
+};
+
+export type getUserParams = {
+	_id?: string;
+	credentials?: {
+		email: string;
+		password: string;
+	};
+};
+
+export type UpdateUserParams = {
+	firstName: string;
+	lastName: string;
+	username: string;
+	password: string;
+	photo: string;
+};
