d714ec0c2adcdf3a15c491cf7f837f945fdc9bd3
Swapnil Kotkar <swapnilkotkar793@gmail.com>
2024-09-13 14:14:19 +0530

    updated user schema

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,14 +1,17 @@
 import { Schema, model, models } from "mongoose";
 
-const UserSchema = new Schema({
-	// firstName: { type: String, required: true },
-	// lastName: { type: String, required: true },
-	email: { type: String, required: true, unique: true },
-	username: { type: String, unique: true },
-	password: { type: String, required: true },
-	photo: { type: String },
-	// onboarding: { type: Boolean, default: false },
-});
+const UserSchema = new Schema(
+	{
+		email: { type: String, required: true, unique: true },
+		username: { type: String, unique: true },
+		password: { type: String, required: true },
+		photo: { type: String },
+		onboarding: { type: Boolean },
+	},
+	{
+		timestamps: true,
+	}
+);
 
 const User = models?.User || model("User", UserSchema);
 
