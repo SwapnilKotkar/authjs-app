f1dcd0a7c6290140523fab855daaca7348e17485
Swapnil Kotkar <swapnilkotkar793@gmail.com>
2024-09-16 00:26:28 +0530

    updated next config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	webpack: (config) => {
+		return config;
+	},
 	experimental: {
 		esmExternals: "loose", // Enable loose mode for ESM externals
 		serverComponentsExternalPackages: ["mongoose"], // Ensure mongoose is treated as an external package in server components
