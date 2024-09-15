64c1b6d14f3600eaac1723bb009bc4a451fb3c6b
Swapnil Kotkar <swapnilkotkar793@gmail.com>
2024-09-15 23:54:02 +0530

    updated next config for mongoose edge runtime issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	webpack: (config) => {
+		config.resolve.alias = {
+			...config.resolve.alias,
+			mongoose: path.resolve("node_modules/mongoose"), // Define alias for mongoose
+		};
+		return config;
+	},
 };
 
 export default nextConfig;
