2abeed9712961f05a0f829d50b88a3b25cf9fd29
Swapnil Kotkar <swapnilkotkar793@gmail.com>
2024-09-15 23:59:15 +0530

    updated db page

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
-import path from "path";
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	webpack: (config) => {
-		config.resolve.alias = {
-			...config.resolve.alias,
-			mongoose: path.resolve("node_modules/mongoose"), // Define alias for mongoose
-		};
-		return config;
-	},
-};
+const nextConfig = {};
 
 export default nextConfig;
