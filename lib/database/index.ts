4acb9353b231053d5783e9cc0405b3643345eca5
Swapnil Kotkar <swapnilkotkar793@gmail.com>
2024-09-15 23:44:50 +0530

    updated db page

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,5 +1,4 @@
-export const runtime = "nodejs"; // optional
-import mongoose from "mongoose";
+export const runtime = "nodejs";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 const DB_NAME = process.env.DB_NAME;
@@ -8,23 +7,25 @@ let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
 	if (cached.conn) {
-		// If already connected, return the existing connection
 		return cached.conn;
 	}
 
 	if (!MONGODB_URI) {
 		throw new Error("MONGODB_URI is missing");
 	}
+
 	if (!DB_NAME) {
 		throw new Error("DB_NAME is missing");
 	}
 
 	if (!cached.promise) {
-		// If no existing promise, create a new connection
+		// Dynamically import mongoose only when the environment allows it
+		const mongoose = await import("mongoose");
+
 		cached.promise = mongoose
 			.connect(MONGODB_URI, {
 				dbName: DB_NAME,
-				bufferCommands: false, // Make sure to disable command buffering
+				bufferCommands: false,
 			})
 			.then((mongooseConnection) => {
 				console.log(`Connected to MongoDB: ${MONGODB_URI}`);
@@ -36,7 +37,6 @@ export const connectToDatabase = async () => {
 			});
 	}
 
-	// Wait for the connection to resolve
 	cached.conn = await cached.promise;
 	return cached.conn;
 };
